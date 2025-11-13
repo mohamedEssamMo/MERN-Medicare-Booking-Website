a0990933f6e1bb883dd0b3ca71e14c7bb5759708
Fares Essam <fares3584071@gmail.com>
2025-11-14 00:13:11 +0200

    fix frontend for vercel (app.css and tailwind.config.js)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,9 +11,8 @@ export default {
         headingColor: "#181A1E",
         textColor: "#4E545F",
       },
-
       boxShadow: {
-        panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;",
+        panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px",
       },
     },
   },
