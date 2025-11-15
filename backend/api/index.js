dcc22eed6f3b6eaddb5a60ce9a76fd064284b520
Fares Essam <fares3584071@gmail.com>
2025-11-15 20:50:34 +0200

    try listen

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,52 +1,52 @@
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser'; 
+import cors from 'cors';
 import mongoose from "mongoose";
-import serverless from "serverless-http";
+import authRoute from '../Routes/auth.js';
+import userRoute from '../Routes/user.js';
+import doctorRoute from '../Routes/doctor.js';
+import reviewRoute from '../Routes/review.js';
+import bookingRoute from '../Routes/booking.js';
 
-import authRoute from "../Routes/auth.js";
-import userRoute from "../Routes/user.js";
-import doctorRoute from "../Routes/doctor.js";
-import reviewRoute from "../Routes/review.js";
-import bookingRoute from "../Routes/booking.js";
+
+
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-// Middleware
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ origin: true }));
-
-// Routes
-app.get("/", (req, res) => {
-  res.send("API is running....");
+const corsOptions = {
+  origin: true,
+}
+app.get('/', (req, res) => {
+  res.send('API is running....');
 });
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/users", userRoute);
-app.use("/api/v1/doctors", doctorRoute);
-app.use("/api/v1/reviews", reviewRoute);
-app.use("/api/v1/bookings", bookingRoute);
+// Connect to MongoDB
 
-// MongoDB connection
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully");
+    console.log('MongoDB connected successfully');
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error('Error connecting to MongoDB:', error);
   }
-};
-connectDB();
-
-// Export as serverless function
-export const handler = serverless(app);
-
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
 }
-export default app;
+//middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use('/api/v1/auth', authRoute)
+app.use('/api/v1/users', userRoute)
+app.use('/api/v1/doctors', doctorRoute)
+app.use('/api/v1/reviews', reviewRoute)
+app.use('/api/v1/bookings', bookingRoute);
+
+
+
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL ="http://localhost:5000/api/v1"
+export const BASE_URL =
+  "https://mern-medicare-booking-website-eshh.vercel.app/api/v1";
 export const token = localStorage.getItem("token")
